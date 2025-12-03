687c59cc43d02e48de4c641fbe736b4d1001bcb4
Rounak Mishra <161303817+RounakMishra06@users.noreply.github.com>
2025-12-04 00:06:45 +0530

    Time: 4 ms (3.81%) | Memory: 44.1 MB (7.78%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            Arrays.sort(nums);
+        }
+        
+    }
+}
