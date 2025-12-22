d3bfe3ae259309c94ef75a6761135abbf51a659b
Rounak Mishra <161303817+RounakMishra06@users.noreply.github.com>
2025-12-22 10:02:41 +0530

    Time: 26 ms (29.92%) | Memory: 46.2 MB (56.66%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,17 +1,49 @@
-class Solution {
-    public int coinChange(int[] coins, int amount) {
-         int[] dp = new int[amount + 1];
-         for (int i = 1; i <= amount; i++) {
-            dp[i] = amount + 1;
-        }
-          dp[0] = 0; 
+// class Solution {
+//     public int coinChange(int[] coins, int amount) {
+//          int[] dp = new int[amount + 1];
+//          for (int i = 1; i <= amount; i++) {
+//             dp[i] = amount + 1;
+//         }
+//           dp[0] = 0; 
     
-    for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (coin <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-                }}
+//     for (int i = 1; i <= amount; i++) { 
+//             for (int coin : coins) {
+//                 if (coin <= i) {
+//                     dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+//                 }}
+//             }
+//             return dp[amount] > amount ? -1 : dp[amount];
+//         }
+// }
+
+class Solution {
+
+    int[] dp;
+
+    public int coinChange(int[] coins, int amount) {
+        dp = new int[amount + 1];
+        for (int i = 0; i <= amount; i++) dp[i] = -2; // -2 = unvisited
+
+        return solve(coins, amount);
+    }
+
+    int solve(int[] coins, int amt) {
+
+        if (amt == 0) return 0;
+        if (amt < 0) return -1;
+
+        if (dp[amt] != -2) return dp[amt];
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int c : coins) {
+            int res = solve(coins, amt - c);
+            if (res >= 0) {
+                ans = Math.min(ans, res + 1);
             }
-            return dp[amount] > amount ? -1 : dp[amount];
         }
+
+        dp[amt] = (ans == Integer.MAX_VALUE) ? -1 : ans;
+        return dp[amt];
+    }
 }
