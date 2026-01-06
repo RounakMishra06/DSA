a0c4ad9f459ca87deeb6685088a3da537daa80dd
Rounak Mishra <161303817+RounakMishra06@users.noreply.github.com>
2026-01-06 20:31:01 +0530

    Time: 4 ms (98.51%) | Memory: 46.5 MB (77.99%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,40 +1,26 @@
 class Solution {
     public String frequencySort(String s) {
-
-        int[] freq = new int[256];
-
-        // Step 1: Count frequency
-        for (char ch : s.toCharArray()) {
-            freq[ch]++;
+        int[] fr = new int[256];
+        for(char ch : s.toCharArray()){
+            fr[ch]++;
         }
-
-        StringBuilder result = new StringBuilder();
-
-        // Step 2: Brute force selection
-        while (true) {
-            int maxFreq = 0;
-            char maxChar = 0;
-
-            // Find character with maximum frequency
-            for (int i = 0; i < 256; i++) {
-                if (freq[i] > maxFreq) {
-                    maxFreq = freq[i];
-                    maxChar = (char) i;
-                }
+          StringBuilder result = new StringBuilder();
+      while(true){
+        int mf = 0;
+        char mc = 0;
+        for(int i = 0;i<256;i++){
+            if(fr[i]>mf){
+                mf=fr[i];
+                mc=(char)i;
             }
-
-            // If no character left
-            if (maxFreq == 0) break;
-
-            // Append that character maxFreq times
-            for (int i = 0; i < maxFreq; i++) {
-                result.append(maxChar);
-            }
-
-            // Mark used
-            freq[maxChar] = 0;
         }
+        if(mf==0) break;
+         for (int i = 0; i < mf; i++) {
+                result.append(mc);
+            }
+            fr[mc]=0;
 
-        return result.toString();
+      }
+      return result.toString();
     }
 }
