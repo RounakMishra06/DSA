e39f8d56935a049968b19832af8d55531b17ef8f
Rounak Mishra <161303817+RounakMishra06@users.noreply.github.com>
2026-01-07 21:00:32 +0530

    Time: 4 ms (57.66%) | Memory: 43.3 MB (84.73%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,22 +1,47 @@
-class Solution {
-    public String removeOuterParentheses(String s) {
-        int cnt = 0;
-        StringBuilder ans = new StringBuilder();
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ')') {
-                cnt--;
+    class Solution {
+        public String removeOuterParentheses(String s) {
+        
+            StringBuilder result = new StringBuilder();
+            int l = 0;
+            for (int i = 0; i < s.length(); i++) {
+                char ch = s.charAt(i);
+            
+                if(ch == '('){
+                    if(l>0){
+                
+                    result.append(ch);
+                    }
+                    l++;
+                    
+                }else{
+                        l--;  
+                if (l > 0) {
+                        result.append(ch);
+                    }
+                }
             }
 
-            if (cnt != 0) {
-                ans.append(s.charAt(i));
+            return result.toString();
+                }
             }
 
-            if (s.charAt(i) == '(') {
-                cnt++;
-            }
-        }
+//         int cnt = 0;
+//         StringBuilder ans = new StringBuilder();
 
-        return ans.toString();
-    }
-}
+//         for (int i = 0; i < s.length(); i++) {
+//             if (s.charAt(i) == ')') {
+//                 cnt--;
+//             }
+
+//             if (cnt != 0) {
+//                 ans.append(s.charAt(i));
+//             }
+
+//             if (s.charAt(i) == '(') {
+//                 cnt++;
+//             }
+//         }
+
+//         return ans.toString();
+//     }
+// }
