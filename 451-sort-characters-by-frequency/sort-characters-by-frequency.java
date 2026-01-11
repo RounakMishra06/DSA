8b8affd6eb3961a78a9f36ee4e25a42f1da7a282
Rounak Mishra <161303817+RounakMishra06@users.noreply.github.com>
2026-01-11 16:47:30 +0530

    Time: 4 ms (98.60%) | Memory: 46.6 MB (67.17%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,26 +1,35 @@
 class Solution {
     public String frequencySort(String s) {
         int[] fr = new int[256];
-        for(char ch : s.toCharArray()){
+
+    
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
             fr[ch]++;
         }
-          StringBuilder result = new StringBuilder();
-      while(true){
-        int mf = 0;
-        char mc = 0;
-        for(int i = 0;i<256;i++){
-            if(fr[i]>mf){
-                mf=fr[i];
-                mc=(char)i;
+
+        StringBuilder result = new StringBuilder();
+
+        while (true) {
+            int mf = 0;
+            char mc = 0;
+
+            for (int i = 0; i < 256; i++) {
+                if (fr[i] > mf) {
+                    mf = fr[i];
+                    mc = (char) i;
+                }
             }
-        }
-        if(mf==0) break;
-         for (int i = 0; i < mf; i++) {
+
+            if (mf == 0) break;
+
+            for (int i = 0; i < mf; i++) {
                 result.append(mc);
             }
-            fr[mc]=0;
 
-      }
-      return result.toString();
+            fr[mc] = 0; 
+        }
+
+        return result.toString();
     }
 }
