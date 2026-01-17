c78c0a924b24a7e290c352e316cab965936a6064
Rounak Mishra <161303817+RounakMishra06@users.noreply.github.com>
2026-01-17 19:41:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (38.01%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -6,12 +6,31 @@ class Solution {
 
         return dp[n] =  fib(n-1)+fib(n-2);
     }
+
+
+    public int tab(int n) {
+        if(n==0) return 0;
+        int[] dp = new int[n+1];
+
+        dp[1] = 1;
+        for(int i=2;i<=n;i++)
+        {
+
+            dp[i] =  dp[i-1] + dp[i-2];
+
+        }
+
+        return dp[n];
+
+
+    }
     
     
   
     public int fib(int n) {
 
-        int[] arr = new int[n+1];
-        return dp(n , arr);
+        // int[] arr = new int[n+1];
+        // return dp(n , arr);
+        return tab(n);
     }
 }
